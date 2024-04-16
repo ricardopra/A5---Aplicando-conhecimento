c21a33c723f317a8c3ca8bcebf77fd38b256acf4
ricardopra <ricardopra@yahoo.com.br>
2024-04-16 20:04:20 -0300

    Novo Exercicio

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const nome = 'Jose Pereira <br>'
+const ano = 'Ano - 2020 <br>'
+const twitter = 'Twitter: @josepereira <br>'
+const facebook = 'Facebook: @josepereira <br>'
+const instagram = 'Instagram: @josepereira <br>'
+const linkedin = 'Linkedin: @josepereira <br>'
+const sexo = 'Sexo - Masculino <br>'
+const localizacao = 'Localização - Polo Higienopolis'
+
+document.write(nome)
+document.write(ano)
+document.write(twitter)
+document.write(facebook)
+document.write(instagram)
+document.write(linkedin)
+document.write(sexo)
+document.write(localizacao)
+
